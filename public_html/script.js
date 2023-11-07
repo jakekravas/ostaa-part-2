11092732194a25ec5bd1f0029803c51af700c13a
Jake Kravas <jakek237@arizona.edu>
2023-11-07 04:35:38 -0700

    added login and signup

---
+++ public_html/script.js
@@ -0,0 +1,117 @@
+/* Jake Kravas
+This file sends user-inputted data, such as user and item data
+to server.js
+*/
+
+let userForm = document.getElementById('user-form');
+let itemForm = document.getElementById('item-form');
+let loginForm = document.getElementById('login-form');
+let signupForm = document.getElementById('signup-form');
+
+loginForm.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  let usernameVal = document.getElementById('login-username').value;
+  let passwordVal = document.getElementById('login-password').value;
+
+  console.log(usernameVal)
+  console.log(passwordVal)
+
+  // object to send to front-end
+  const info = {
+    username: usernameVal,
+    password: passwordVal
+  }
+
+  // let checkCredentials = fetch(`/login/${usernameVal}/${passwordVal}`);
+
+  // checkCredentials.then( (response) => {
+  //   return response.json();
+
+  // }).then((userObject) => {
+
+  //   if (userObject) {
+  //     console.log('valid')
+  //     console.log(userObject)
+  //     document.cookie = `username=${userObject.username}`;
+  //   } else {
+  //     console.log('invalid')
+  //     document.cookie = `username=`;
+  //   }
+
+  // }).catch(() => { 
+  //   alert('something went wrong');
+  // });
+
+
+
+
+  let post = fetch('/login', {
+    method: 'POST',
+    body: JSON.stringify(info),
+    headers: { 'Content-Type': 'application/json'}
+  });
+
+  post.then(response => {
+    return response.json();
+
+  }).then((userObject) => {
+
+    if (userObject) {
+      console.log('valid')
+      console.log(userObject)
+      document.cookie = `username=${userObject.username}`;
+    } else {
+      console.log('invalid')
+      document.cookie = `username=`;
+    }
+
+  }).catch(() => { 
+    alert('something went wrong');
+  });
+
+});
+
+
+
+// Create and save user
+signupForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  let usernameVal = document.getElementById('signup-username').value;
+  let passwordVal = document.getElementById('signup-password').value;
+
+  console.log(usernameVal)
+  console.log(passwordVal)
+
+   // if both username and password are not empty, send them to back-end
+   if (usernameVal && passwordVal) {
+
+    // object to send to front-end
+    const info = {
+      username: usernameVal,
+      password: passwordVal
+    }
+
+    let post = fetch('/add/user', {
+      method: 'POST',
+      body: JSON.stringify(info),
+      headers: { 'Content-Type': 'application/json'}
+    });
+
+    post.then(response => {
+
+      document.getElementById('signup-username').value = '';
+      document.getElementById('signup-password').value = '';
+      // response.text();
+      return response.json()
+      // console.log(response.text())
+      // window.location.href = 'http://127.0.0.1:8080/get/users';
+    }).then((text) => {
+      console.log(text)
+    }).catch(() => {
+      console.log('Something went wrong')
+    });
+  }
+});
+
---
+++ server.js
@@ -0,0 +1,306 @@
+/* Jake Kravas
+This file receives data from the front end,
+and adds users and items to the database
+*/
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const bp = require('body-parser');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// Database stuff
+const MONGODB_URL = 'mongodb+srv://jake:cmXhjrj9ZzDRlmHz@cluster0.pd6qpel.mongodb.net/?retryWrites=true&w=majority';
+
+let Schema = mongoose.Schema;
+
+let ItemSchema = new Schema({
+  title: String,
+  description: String,
+  image: String,
+  price: Number,
+  stat: String
+});
+
+let UserSchema = new Schema({
+  username: String,
+  password: String,
+  listings: [String],
+  purchases: [String]
+});
+
+let Item = mongoose.model('item', ItemSchema );
+let User = mongoose.model('user', UserSchema );
+
+// Starts database
+const db = async() => {
+  try {
+    const con = await mongoose.connect(MONGODB_URL);
+    console.log('Database Connected');
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+db()
+
+app.use(express.static('/assets'), express.static(__dirname + '/public_html'));
+app.use(cookieParser());
+app.use(bp.json());
+app.use(cors());
+
+// Starts app
+app.listen(port, () =>
+  console.log(`App listening at http://127.0.0.1:${port}`)
+);
+
+// get all users
+app.get('/get/users', async function(req, res) {
+  let users = await User.find({});
+  let usersObj = JSON.stringify(users);
+  
+  res.end(usersObj);
+});
+
+// get all items
+app.get('/get/items', async function(req, res) {
+  let items = await Item.find({});
+  let itemsObj = JSON.stringify(items);
+  
+  res.end(itemsObj);
+});
+
+// get all listings of given user
+app.get('/get/listings/:username', async function(req, res) {
+  let user = await User.findOne({username: req.params.username.toLowerCase()});
+  let userListingIDs = user.listings;
+  let userListings = [];
+
+  // for each item id in userListingIDs, retrieve that item from database and push to userListings
+  for (let i = 0; i < userListingIDs.length; i++) {
+    let item = await Item.findById(userListingIDs[i]);
+    if (item) {
+      userListings.push(item);
+    }
+  }
+
+  let listingsObj = JSON.stringify(userListings);
+  
+  res.end(listingsObj);
+});
+
+// get all purchases of given user
+app.get('/get/purchases/:username', async function(req, res) {
+  let user = await User.findOne({username: req.params.username.toLowerCase()});
+  let userPurchasesIDs = user.purchases;
+  let userPurchases = [];
+
+  // for each item id in userPurchasesIDs, retrieve that item from database and push to userPurchases
+  for (let i = 0; i < userPurchasesIDs.length; i++) {
+    let item = await Item.findById(userPurchasesIDs[i]);
+    if (item) {
+      userPurchases.push(item);
+    }
+  }
+
+  let purchasesObj = JSON.stringify(userPurchases);
+  
+  res.end(purchasesObj);
+});
+
+
+// get search results of users based on keyword
+app.get('/search/users/:keyword', async function(req, res) {
+  let users = await User.find({});
+
+  let searchResults = [];
+
+  // for each user, if the keyword is included in their username,
+  // add it to searchResults
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username.includes(req.params.keyword)) {
+      searchResults.push(users[i])
+    }
+  }
+
+  let searchResultsStr = JSON.stringify(searchResults);
+  
+  res.end(searchResultsStr);
+});
+
+// get search items of users based on keyword
+app.get('/search/items/:keyword', async function(req, res) {
+  let items = await Item.find({});
+
+  let searchResults = [];
+
+  // for each item, if the keyword is included in its description,
+  // add it to searchResults
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].description.includes(req.params.keyword)) {
+      searchResults.push(items[i])
+    }
+  }
+
+  let searchResultsStr = JSON.stringify(searchResults);
+  
+  res.end(searchResultsStr);
+});
+
+
+// Save new user
+app.post('/add/user', async function(req, res) {
+  let username = req.body.username.toLowerCase();
+  let password = req.body.password;
+
+  let newUser = new User({
+    username: username,
+    password: password,
+    listings: [],
+    purchases: []
+  });
+
+  let userExists = await User.findOne({username: username}).exec();
+
+  if (userExists == null) {
+    try {
+      await newUser.save();
+      res.send('User saved successfully');
+    } catch (err) {
+      return res.status(500).send('Failed to save user');
+    }
+  } else {
+    res.send('Username already exists');
+  }
+});
+
+// Login user
+app.post('/login', async function(req, res) {
+
+  let username = req.body.username.toLowerCase();
+  let password = req.body.password;
+
+  console.log(username)
+  console.log(password)
+  console.log('HIT')
+
+  // find user
+  let foundUser = await User.findOne({username: username, password: password}).exec();
+
+  console.log(foundUser)
+
+  // if (foundUser) {
+  //   let usersObj = JSON.stringify(foundUser);
+  //   res.send(usersObj);
+  // } else {
+  //     let usersObj = JSON.stringify(foundUser);
+  //     res.send(true);
+  //   }
+    
+  let usersObj = JSON.stringify(foundUser);
+
+  console.log(res.cookie())
+  // console.log(res.cookie().body)
+  // console.log(res.cookie().cookies)
+  console.log(res.req)
+  console.log(res.req.cookies)
+
+  // USER LATER
+  let cookies = res.req.cookies.username;
+
+  res.send(usersObj);
+
+});
+
+
+
+// Save new item
+app.post('/add/item/:username', async function(req, res) {
+
+  let username = req.params.username.toLowerCase();
+  let title = req.body.title;
+  let description = req.body.description;
+  let image = req.body.image;
+  let price = req.body.price;
+  let status = req.body.status;
+
+  let newItem = new Item({
+    title: title,
+    description: description,
+    image: image,
+    price: price,
+    stat: status
+  });
+
+  try {
+    await newItem.save();
+    console.log(newItem.id)
+
+    let user = await User.findOne({username: username});
+
+    // if status is "SALE", save item to user's listings
+    if (status == 'SALE') {
+      let newListings = user.listings;
+      newListings.push(newItem.id);
+  
+      User.updateOne(
+        { _id: user.id },
+        { $set: {listings: newListings} }
+      ).exec();
+
+    // if status is "SOLD", save item to user's purchases
+    } else if (status == 'SOLD') {
+
+      let newPurchases = user.purchases;
+      newPurchases.push(newItem.id);
+  
+      User.updateOne(
+        { _id: user.id },
+        { $set: {purchases: newPurchases} }
+      ).exec();
+    }
+
+    // await newItem.save();
+    return res.send('Item saved successfully');
+  } catch (err) {
+    return res.status(500).send('Failed to save item');
+  }
+});
+
+
+
+// check login credentials
+// app.get('/login', async function(req, res) {
+// app.get('/login/:username/:password', async function(req, res) {
+
+//   let username = req.params.username.toLowerCase();
+//   let password = req.params.password;
+
+//   console.log(username)
+//   console.log(password)
+//   console.log('HIT')
+
+//   // find user
+//   let foundUser = await User.findOne({username: username, password: password}).exec();
+
+//   console.log(foundUser)
+
+//   // if (foundUser) {
+//   //   let usersObj = JSON.stringify(foundUser);
+//   //   res.send(usersObj);
+//   // } else {
+//     //   let usersObj = JSON.stringify(foundUser);
+//     //   res.send(true);
+//     // }
+    
+    
+//   let usersObj = JSON.stringify(foundUser);
+
+//   console.log(res.cookie().params)
+//   // console.log(res.cookies)
+
+//   res.send(usersObj);
+
+// });
